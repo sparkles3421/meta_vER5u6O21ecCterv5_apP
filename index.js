d11dc05743f81c4bcb8c4d214df2473bc8440b7b
sparkles3421 <101483586+sparkles3421@users.noreply.github.com>
2023-03-02 21:27:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
+    console.log(`Request to > root ${req.url}!`)
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
