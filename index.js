5e31641d774280980b25ffe397e213ab0fed6219
sparkles3421 <101483586+sparkles3421@users.noreply.github.com>
2023-03-02 21:44:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 var http = require('http');
-const fetch = require('node-fetch');
-var jsondata = [];
-
-fetch("https://raw.githubusercontent.com/sparkles3421/meta_vER5u6O21ecCterv5_apP/main/data.json", {method:"Get"})
-    .then(res => res.json())
-    .then((dataget) => {
-        jsondata = dataget;
-    });
-
 http.createServer(function (req, res) {
     console.log(`Request to > root ${req.url}!`)
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(JSON.stringify(jsondata, null, 3));
+    res.write('Requested!');
+    res.end();
 }).listen(process.env.PORT || 3000);
