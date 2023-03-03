d37d19ae780363721e43858d8d28543b58852c81
sparkles3421 <101483586+sparkles3421@users.noreply.github.com>
2023-03-03 19:32:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ http.createServer(function (req, res) {
       res.write(JSON.stringify({"STATUS":404,"ERROR":"Invalid path"},null,3));
     }
     if(process.env.Latest==thisVER){
-      res.redirect(req.get('referer'));
+      res.redirect(req.url);
     }
     res.end();
 }).listen(process.env.PORT || 3000);
