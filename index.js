8c42eb83ea9b835010a7a281a57a04b3f8bbaf76
sparkles3421 <101483586+sparkles3421@users.noreply.github.com>
2023-03-03 19:52:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,12 @@ var data = {
   "STATUS":200,
   "ERROR":"null"
 }
-http.get('/client.js', function(req, res) {
-  res.sendFile(path.join(__dirname + '/client.js'));
-});
 http.createServer(function (req, res) {
     console.log(`Request to > root${req.url}`)
     if(req.url=="/")  {
       res.write(JSON.stringify(data, null, 3));
     } else if(req.url=="/client.js"){
-      res.write(
-                  fs.readFileSync("script.js", "utf8")
-               )
+      res.write(fs.readFileSync("client.js", "utf8"))
     } else {
       res.write(JSON.stringify({"STATUS":404,"ERROR":"Invalid path"},null,3));
     }
