a21e76d8cb36f926f36a1fa33ac84c2c60b342d9
sparkles3421 <101483586+sparkles3421@users.noreply.github.com>
2023-03-03 19:09:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var data = {
 }
 http.createServer(function (req, res) {
     console.log(`Request to > root ${req.url}!`)
-    res.write(JSON.stringify(data, null, 3));
+    if(req.url=="/!")  {
+      res.write(JSON.stringify(data, null, 3));
+    } else {
+      res.write(JSON.stringify({"STATUS":404,"ERROR":"Invalid path"
+    }
     res.end();
 }).listen(process.env.PORT || 3000);
