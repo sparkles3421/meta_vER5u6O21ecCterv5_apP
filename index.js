801792860678c6600f269c5d581179d78768b6d5
sparkles3421 <101483586+sparkles3421@users.noreply.github.com>
2023-03-03 19:49:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const thisVER = process.env.Latest;
+var fs   = require('fs')
 var http = require('http');
 var data = {
   "STATUS":200,
@@ -11,6 +12,10 @@ http.createServer(function (req, res) {
     console.log(`Request to > root${req.url}`)
     if(req.url=="/")  {
       res.write(JSON.stringify(data, null, 3));
+    } else if(req.url=="/client.js"){
+      res.write(
+                  fs.readFileSync("script.js", "utf8")
+               )
     } else {
       res.write(JSON.stringify({"STATUS":404,"ERROR":"Invalid path"},null,3));
     }
